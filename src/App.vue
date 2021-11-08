efe23a1862a68582bce7b9ce3c55119030a429cd
zhongjunji <zhongjunji@jd.com>
2021-11-08 15:54:50 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Test msg="first-message" />
+  </div>
+</template>
+<script>
+import Test from './components/test.vue'
+export default {
+  components: {
+    Test,
+  },
+  
+}
+</script>
+<style>
+  
+</style>
