3b3453f118dd963a5d872735827f54e426de8f1c
wlin00 <wlin0z@163.com>
2022-08-29 02:51:38 +0800

    fix

--- src/utils/useCountDown.ts
+++ src/utils/useCountDown.ts
@@ -2,8 +2,8 @@ import { onMounted, onUnmounted, ref } from 'vue';
 // 参数1: 倒计时时间， 默认值为60秒
 // 参数2: manually 是否手动触发倒计时，默认为false
 function useCountDown (seconds: number = 60, manually?: boolean) { // 倒计时hook，返回当前计时count、pending状态flag、手动触发方法startCountDown
-  const timer = ref<number>()
-  const count = ref(seconds)
+  const timer = ref(null)
+  const count = ref<number>(seconds)
   const pending = ref<boolean>(false)
 
   function startCountDown() {
